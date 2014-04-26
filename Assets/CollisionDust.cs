72ddd0ebd2ca4fa5bf72abd14d753642cda8dfdc
David W <davidw@danvil.de>
2014-04-26 20:28:50 +0200

    Added dust effects

---
+++ Assets/CollisionDust.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionDust : MonoBehaviour {
+
+	public GameObject pfDustFx;
+	public float cooldown = 1.0f;
+
+	float currentCooldown = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentCooldown -= Time.deltaTime;
+	}
+
+	void OnCollisionStay(Collision collisionInfo) {
+		foreach (ContactPoint contact in collisionInfo.contacts) {
+			Debug.DrawRay(contact.point, contact.normal, Color.white);
+		}
+		if(currentCooldown <= 0 && collisionInfo.contacts.Length > 0) {
+			this.currentCooldown = this.cooldown;
+			GameObject go = (GameObject)Instantiate(pfDustFx);
+			go.transform.position = collisionInfo.contacts[0].point;
+			Destroy(go, 5.0f);
+		}
+	}
+}
